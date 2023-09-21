5d9aa628209faebb2874cf30f1fdd646b4782b5d
Nikolaus-Alexander <alexandernikolaus04@gmail.com>
2023-09-21 11:29:05 +0200

    Unfertiges Package und PackageBody

--- Arztpraxis.sql
+++ Arztpraxis.sql
@@ -1,12 +1,67 @@
 CREATE OR REPLACE
-PACKAGE ARZTPRAXIS AS
+    PACKAGE ARZTPRAXIS AS
 
-FUNCTION InsertPatientRecord(SVN number) return BOOLEAN;
+    /*ALLE PARAMETER NUR ALS SAMPLE CODE*/
 
-PROCEDURE GeneratePracticeOverviewReport(PraxisID NUMBER);
+    FUNCTION InsertPatientRecord(SVN number) return BOOLEAN;
 
-FUNCTION ScheduleAppointment(SVN number, datum DATE) RETURN BOOLEAN;
+    PROCEDURE GeneratePracticeOverviewReport(PraxisID NUMBER);
 
+    PROCEDURE ScheduleAppointment(SVN number, datum DATE);
 
+    PROCEDURE RecordMedicalService(SVN number, datum DATE);
+
+    FUNCTION GetPatientDemographicsStatistics(PraxisID NUMBER) return number;
+
+    FUNCTION GetCommonDiagnosesStatistics(PraxisID NUMBER) return number;
+
+    FUNCTION GetDoctorPerformanceStatistics (ArztID number) return number;
+
+END ARZTPRAXIS;
+
+CREATE OR REPLACE
+PACKAGE BODY ARZTPRAXIS AS
+
+  FUNCTION InsertPatientRecord(SVN number) return BOOLEAN AS
+  BEGIN
+    -- TODO: Implementierung für FUNCTION ARZTPRAXIS.InsertPatientRecord erforderlich
+    RETURN NULL;
+  END InsertPatientRecord;
+
+  PROCEDURE GeneratePracticeOverviewReport(PraxisID NUMBER) AS
+  BEGIN
+    -- TODO: Implementierung für PROCEDURE ARZTPRAXIS.GeneratePracticeOverviewReport erforderlich
+    NULL;
+  END GeneratePracticeOverviewReport;
+
+  PROCEDURE ScheduleAppointment(SVN number, datum DATE) AS
+  BEGIN
+    -- TODO: Implementierung für PROCEDURE ARZTPRAXIS.ScheduleAppointment erforderlich
+    NULL;
+  END ScheduleAppointment;
+
+  PROCEDURE RecordMedicalService(SVN number, datum DATE) AS
+  BEGIN
+    -- TODO: Implementierung für PROCEDURE ARZTPRAXIS.RecordMedicalService erforderlich
+    NULL;
+  END RecordMedicalService;
+
+  FUNCTION GetPatientDemographicsStatistics(PraxisID NUMBER) return number AS
+  BEGIN
+    -- TODO: Implementierung für FUNCTION ARZTPRAXIS.GetPatientDemographicsStatistics erforderlich
+    RETURN NULL;
+  END GetPatientDemographicsStatistics;
+
+  FUNCTION GetCommonDiagnosesStatistics(PraxisID NUMBER) return number AS
+  BEGIN
+    -- TODO: Implementierung für FUNCTION ARZTPRAXIS.GetCommonDiagnosesStatistics erforderlich
+    RETURN NULL;
+  END GetCommonDiagnosesStatistics;
+
+  FUNCTION GetDoctorPerformanceStatistics (ArztID number) return number AS
+  BEGIN
+    -- TODO: Implementierung für FUNCTION ARZTPRAXIS.GetDoctorPerformanceStatistics erforderlich
+    RETURN NULL;
+  END GetDoctorPerformanceStatistics;
 
 END ARZTPRAXIS;
