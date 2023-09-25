a7d463cdf26b057b21be05e78175adede1f9940d
davidlehner1 <d.lehner2@students.htl-leonding.ac.at>
2023-09-25 20:51:18 +0200

    - fix: overworked ddl, and created table drops

---
+++ SQLFiles/dropTables.sql
@@ -0,0 +1,23 @@
+drop table TERMIN
+/
+
+drop table ARZT
+/
+
+drop table ARZTPRAXIS
+/
+
+drop table ARZTTYP
+/
+
+drop table BEHANDLUNG
+/
+
+drop table DIAGNOSE
+/
+
+drop table REZEPT
+/
+
+drop table PATIENT
+/
--- SQLFiles/genereateTables.sql
+++ SQLFiles/genereateTables.sql
@@ -1,72 +1,86 @@
-
 CREATE TABLE arztpraxis
 (
-    praxisid number PRIMARY KEY,
-    plz      varchar2(10),
-    ort      varchar2(255),
-    adresse  varchar2(255),
-    hausnr   varchar2(10)
+    praxisid number(20)   NOT NULL,
+    plz      varchar2(10)  NOT NULL,
+    ort      varchar2(255) NOT NULL,
+    adresse  varchar2(255) NOT NULL,
+    hausnr   varchar2(10)  NOT NULL,
+    CONSTRAINT pk_arztpraxis PRIMARY KEY (praxisid)
 );
 
 CREATE TABLE arzttyp
 (
-    typid        number PRIMARY KEY,
-    name         varchar2(255),
-    beschreibung varchar2(255)
+    typid        number(20)   NOT NULL,
+    name         varchar2(255) NOT NULL,
+    beschreibung varchar2(255) NOT NULL,
+    CONSTRAINT pk_arzttyp PRIMARY KEY (typid)
 );
 
 CREATE TABLE arzt
 (
-    arztid    number PRIMARY KEY,
-    vname     varchar2(255),
-    nname     varchar2(255),
-    titel     varchar2(50),
-    arzttypfk number REFERENCES arzttyp(typid),
-    praxisfk  number REFERENCES arztpraxis(praxisid)
+    arztid    number(20)   NOT NULL,
+    vname     varchar2(255) NOT NULL,
+    nname     varchar2(255) NOT NULL,
+    titel     varchar2(50)  NOT NULL,
+    arzttypfk number(20)   NOT NULL,
+    praxisfk  number(20)   NOT NULL,
+    CONSTRAINT pk_arzt PRIMARY KEY (arztid),
+    CONSTRAINT fk_arzt_arzttyp FOREIGN KEY (arzttypfk) REFERENCES arzttyp (typid),
+    CONSTRAINT fk_arzt_praxis FOREIGN KEY (praxisfk) REFERENCES arztpraxis (praxisid)
 );
 
 CREATE TABLE patient
 (
-    svn     varchar2(20) PRIMARY KEY,
-    vname   varchar2(255),
-    nname   varchar2(255),
-    plz     varchar2(10),
-    ort     varchar2(255),
-    adresse varchar2(255),
-    hausnr  varchar2(10),
-    geb     date
+    svn     varchar2(20)  NOT NULL,
+    vname   varchar2(255) NOT NULL,
+    nname   varchar2(255) NOT NULL,
+    plz     varchar2(10)  NOT NULL,
+    ort     varchar2(255) NOT NULL,
+    adresse varchar2(255) NOT NULL,
+    hausnr  varchar2(10)  NOT NULL,
+    geb     date          NOT NULL,
+    CONSTRAINT pk_patient PRIMARY KEY (svn)
 );
 
 CREATE TABLE termin
 (
-    terminid  number PRIMARY KEY,
-    datum     date,
-    uhrzeit   number,
-    dauer     number,
-    patientfk varchar2(20) REFERENCES patient(svn),
-    arztfk    number REFERENCES arzt(arztid)
+    terminid  number(20)  NOT NULL,
+    datum     date         NOT NULL,
+    uhrzeit   number(20)  NOT NULL,
+    dauer     number(20)  NOT NULL,
+    patientfk varchar2(20) NOT NULL,
+    arztfk    number(20)  NOT NULL,
+    CONSTRAINT pk_termin PRIMARY KEY (terminid),
+    CONSTRAINT fk_termin_patient FOREIGN KEY (patientfk) REFERENCES patient (svn),
+    CONSTRAINT fk_termin_arzt FOREIGN KEY (arztfk) REFERENCES arzt (arztid)
 );
 
 CREATE TABLE diagnose
 (
-    diagnoseid   number PRIMARY KEY,
-    name         varchar2(255),
-    beschreibung varchar2(255)
+    diagnoseid   number(20)   NOT NULL,
+    name         varchar2(255) NOT NULL,
+    beschreibung varchar2(255) NOT NULL,
+    CONSTRAINT pk_diagnose PRIMARY KEY (diagnoseid)
 );
 
 CREATE TABLE rezept
 (
-    rezeptid          number PRIMARY KEY,
-    verschreibung     varchar2(255),
-    ausstellungsdatum date,
-    preis             decimal(10, 2),
-    patientfk         varchar2(20) REFERENCES patient(svn)
+    rezeptid          number(20)    NOT NULL,
+    verschreibung     varchar2(255)  NOT NULL,
+    ausstellungsdatum date           NOT NULL,
+    preis             decimal(10, 2) NOT NULL,
+    patientfk         varchar2(20)   NOT NULL,
+    CONSTRAINT pk_rezept PRIMARY KEY (rezeptid),
+    CONSTRAINT fk_rezept_patient FOREIGN KEY (patientfk) REFERENCES patient (svn)
 );
 
 CREATE TABLE behandlung
 (
-    behandlungsid  number PRIMARY KEY,
-    rezeptfk       number REFERENCES rezept(rezeptid),
-    diagnosefk     number REFERENCES diagnose(diagnoseid),
-    behandlungsart varchar2(255)
+    behandlungsid  number(20)   NOT NULL,
+    behandlungsart varchar2(255) NOT NULL,
+    rezeptfk       number(20)   NOT NULL,
+    diagnosefk     number(20)   NOT NULL,
+    CONSTRAINT pk_behandlung PRIMARY KEY (behandlungsid),
+    CONSTRAINT fk_behandlung_rezept FOREIGN KEY (rezeptfk) REFERENCES rezept (rezeptid),
+    CONSTRAINT fk_behandlung_diagnose FOREIGN KEY (diagnosefk) REFERENCES diagnose (diagnoseid)
 );
