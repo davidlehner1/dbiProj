c505bdb01555461123788f27f7eb96caca462ab2
Nikolaus-Alexander <alexandernikolaus04@gmail.com>
2023-09-27 11:00:46 +0200

    PACKAGE update

--- SQLFiles/Arztpraxis.sql
+++ SQLFiles/Arztpraxis.sql
@@ -1,16 +1,15 @@
-CREATE OR REPLACE
-    PACKAGE ARZTPRAXIS AS
+create or replace PACKAGE ARZTPRAXIS AS
 
     /*ALLE PARAMETER NUR ALS SAMPLE CODE*/
 
-    FUNCTION InsertPatientRecord(svn varchar2(20),
-                                 vname varchar2(255),
-                                 nname varchar2(255),
-                                 plz varchar2(10),
-                                 ort varchar2(255),
-                                 adresse varchar2(255),
-                                 hausnr varchar2(10),
-                                 geb date) return BOOLEAN;
+    FUNCTION InsertPatientRecord(svn varchar2,
+                                 vname varchar2,
+                                 nname varchar2,
+                                 plz varchar2,
+                                 ort varchar2,
+                                 adresse varchar2,
+                                 hausnr varchar2,
+                                 geb date) return varchar2;
 
     PROCEDURE GeneratePracticeOverviewReport(PraxisID NUMBER);
 
@@ -26,56 +25,71 @@ CREATE OR REPLACE
 
 END ARZTPRAXIS;
 
-    CREATE OR REPLACE
-PACKAGE BODY ARZTPRAXIS AS
+CREATE OR REPLACE
+    PACKAGE BODY ARZTPRAXIS AS
 
-  FUNCTION InsertPatientRecord(svn varchar2,
-                                 vname varchar2,
-                                 nname varchar2,
-                                 plz varchar2,
-                                 ort varchar2,
-                                 adresse varchar2,
-                                 hausnr varchar2,
-                                 geb date) return BOOLEAN AS
-  BEGIN
-    -- TODO: Implementierung für FUNCTION ARZTPRAXIS.InsertPatientRecord erforderlich
-    RETURN NULL;
-  END InsertPatientRecord;
+    FUNCTION InsertPatientRecord(
+        svn varchar2, vname varchar2, nname varchar2, plz varchar2, ort varchar2,
+        adresse varchar2, hausnr varchar2, geb date)
+        return varchar2 AS
+        p_count number;
+    BEGIN
+        -- Prüfen, ob der Patient bereits in der Datenbank existiert
+        SELECT COUNT(SVN)
+        INTO p_count
+        FROM PATIENT
+        WHERE SVN = svn;
 
-  PROCEDURE GeneratePracticeOverviewReport(PraxisID NUMBER) AS
-  BEGIN
-    -- TODO: Implementierung für PROCEDURE ARZTPRAXIS.GeneratePracticeOverviewReport erforderlich
-    NULL;
-  END GeneratePracticeOverviewReport;
+        -- Wenn der Patient nicht existiert, fügen Sie ihn hinzu
+        IF p_count = 0 THEN
+            INSERT INTO PATIENT (SVN, VNAME, NNAME, PLZ, ORT, ADRESSE, HAUSNR, GEB)
+            VALUES (svn, vname, nname, plz, ort, adresse, hausnr, geb);
+            COMMIT; -- Bestätigen Sie die Transaktion
+            RETURN 'TRUE'; -- Erfolgreich eingefügt
+        ELSE
+            RETURN 'FALSE'; -- Patient existiert bereits
+            COMMIT;
+        END IF;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK; -- Bei Fehlern Transaktion rückgängig machen
+            RETURN 'FALSE'; -- Fehler bei der Einfügung
+    END InsertPatientRecord;
 
-  PROCEDURE ScheduleAppointment(SVN number, datum DATE) AS
-  BEGIN
-    -- TODO: Implementierung für PROCEDURE ARZTPRAXIS.ScheduleAppointment erforderlich
-    NULL;
-  END ScheduleAppointment;
+    PROCEDURE GeneratePracticeOverviewReport(PraxisID NUMBER) AS
+    BEGIN
+        -- TODO: Implementierung für PROCEDURE ARZTPRAXIS.GeneratePracticeOverviewReport erforderlich
+        NULL;
+    END GeneratePracticeOverviewReport;
 
-  PROCEDURE RecordMedicalService(SVN number, datum DATE) AS
-  BEGIN
-    -- TODO: Implementierung für PROCEDURE ARZTPRAXIS.RecordMedicalService erforderlich
-    NULL;
-  END RecordMedicalService;
+    PROCEDURE ScheduleAppointment(SVN number, datum DATE) AS
+    BEGIN
+        -- TODO: Implementierung für PROCEDURE ARZTPRAXIS.RecordMedicalService erforderlich
+        NULL;
+    END ScheduleAppointment;
 
-  FUNCTION GetPatientDemographicsStatistics(PraxisID NUMBER) return number AS
-  BEGIN
-    -- TODO: Implementierung für FUNCTION ARZTPRAXIS.GetPatientDemographicsStatistics erforderlich
-    RETURN NULL;
-  END GetPatientDemographicsStatistics;
+    PROCEDURE RecordMedicalService(SVN number, datum DATE) AS
+    BEGIN
+        -- TODO: Implementierung für PROCEDURE ARZTPRAXIS.RecordMedicalService erforderlich
+        NULL;
+    END RecordMedicalService;
 
-  FUNCTION GetCommonDiagnosesStatistics(PraxisID NUMBER) return number AS
-  BEGIN
-    -- TODO: Implementierung für FUNCTION ARZTPRAXIS.GetCommonDiagnosesStatistics erforderlich
-    RETURN NULL;
-  END GetCommonDiagnosesStatistics;
+    FUNCTION GetPatientDemographicsStatistics(PraxisID NUMBER) return number AS
+    BEGIN
+        -- TODO: Implementierung für FUNCTION ARZTPRAXIS.GetPatientDemographicsStatistics erforderlich
+        RETURN NULL;
+    END GetPatientDemographicsStatistics;
 
-  FUNCTION GetDoctorPerformanceStatistics(ArztID number) return number AS
-  BEGIN
-    -- TODO: Implementierung für FUNCTION ARZTPRAXIS.GetDoctorPerformanceStatistics erforderlich
-    RETURN NULL;
-  END GetDoctorPerformanceStatistics;
+    FUNCTION GetCommonDiagnosesStatistics(PraxisID NUMBER) return number AS
+    BEGIN
+        -- TODO: Implementierung für FUNCTION ARZTPRAXIS.GetCommonDiagnosesStatistics erforderlich
+        RETURN NULL;
+    END GetCommonDiagnosesStatistics;
+
+    FUNCTION GetDoctorPerformanceStatistics(ArztID number) return number AS
+    BEGIN
+        -- TODO: Implementierung für FUNCTION ARZTPRAXIS.GetDoctorPerformanceStatistics erforderlich
+        RETURN NULL;
+    END GetDoctorPerformanceStatistics;
 
 END ARZTPRAXIS;
