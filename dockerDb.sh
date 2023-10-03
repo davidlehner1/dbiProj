10472a558172576a4467be2e666de38b58ff8010
FroellerFelix <felix.froeller@gmail.com>
2023-10-03 08:24:43 +0200

    fix: fixed dockerDb.sh not working when container already exists.

--- dockerDb.sh
+++ dockerDb.sh
@@ -1,3 +1,10 @@
-docker run --name oraxe21 -d -p 15210:1521 -e
-ORACLE_PASSWORD=Oraxe23 -v oracledb21:/opt/oracle/oradata
-gvenzl/oracle-xe:slim
+#!/bin/bash
+
+# Check if the container with the name 'oraxe21' already exists
+if [ "$(docker ps -aq -f name=oraxe21)" ]; then
+    # Container exists, so start it
+    docker start oraxe21
+else
+    # Container doesn't exist, so create and start it
+    docker run --name oraxe21 -d -p 15210:1521 -e ORACLE_PASSWORD=Oraxe23 -v oracledb21:/opt/oracle/oradata gvenzl/oracle-xe:slim
+fi
